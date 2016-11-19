3b4870c9c861c75b8eda9c2d9218b74db71b9f24
Elizabeth Murray <lizmurray479@gmail.com>
2016-11-19 15:01:19 -0500

    Updated Tweet selector to capture entire contents of <p> tags. Added Date selector. Captured images. Total build time ~ 1 hr

--- scraper.py
+++ scraper.py
@@ -1,23 +1,29 @@
 import scrapy
 
 
-class BrickSetSpider(scrapy.Spider):
-    name = 'brickset_spider'
-    start_urls = ['file://127.0.0.0.1/home/liz/Downloads/urwhatutweat.html']
+class TwitterSpider(scrapy.Spider):
+    name = 'twitter_spider'
+    start_urls = ['file://127.0.0.0.1/home/liz/Downloads/kirsy.html']
     # start_urls = ['https://twitter.com/search?f=tweets&q=%23urwhatutweat&src=typd']
 
     def parse(self, response):
-        SET_SELECTOR = '.js-stream-item'  # Getting class to pull base info from
-        for brickset in response.css(SET_SELECTOR):
-            NAME_SELECTOR = '.stream-item-header a strong ::text'  # CSS identifier for the set name
+        SET_SELECTOR = '.js-stream-item'
+        for twitter in response.css(SET_SELECTOR):
+            NAME_SELECTOR = '.stream-item-header a strong ::text'
             USER_SELECTOR = '.stream-item-header a .username ::text'
-            CONTENT_SELECTOR = '.content p ::text'
+            CONTENT_SELECTOR = '.content .TweetTextSize  ::text'
+            QUOTE_SELECTOR = '.content .QuoteTweet-text ::text'
+            TIME_SELECTOR = '.content small .tweet-timestamp ::text'
             # IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
-                'name': brickset.css(NAME_SELECTOR).extract_first(),
-                'user': brickset.css(USER_SELECTOR).extract_first(),
-                'minifigs': brickset.css(CONTENT_SELECTOR).extract_first(),
-                # 'image': brickset.css(IMAGE_SELECTOR).extract_first(),
+                'name': twitter.css(NAME_SELECTOR).extract_first(),
+                'user': twitter.css(USER_SELECTOR).extract(),
+                'tweet': twitter.css(CONTENT_SELECTOR).extract(),
+                'date': twitter.css(TIME_SELECTOR).extract_first(),
+                'quote': twitter.css(QUOTE_SELECTOR).extract_first(),
+                # 'image': twitter.css(IMAGE_SELECTOR).extract_first(),
+                # {"name": "Danielle Vucenovic", "quote": null, "time": "Nov 8", "tweet": "Obviously not going to stop ", "user": "@"},
+
             }
         NEXT_PAGE_SELECTOR ='.next a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
